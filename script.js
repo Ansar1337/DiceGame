93db29349e906662f1856d991a40dc90d374c4e9
Ansar <ansarshayekin1@gmail.com>
2025-10-30 15:38:47 +0000

    Add game reset functionality

--- script.js
+++ script.js
@@ -13,14 +13,27 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
-score0_Element.textContent = "0";
-score1_Element.textContent = "0";
-dice_Element.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0_Element.textContent = "0";
+    score1_Element.textContent = "0";
+    current0_Element.textContent = "0";
+    current1_Element.textContent = "0";
+
+    dice_Element.classList.add("hidden");
+    player0_Element.classList.remove("player--winner");
+    player1_Element.classList.remove("player--winner");
+    player0_Element.classList.add("player--active");
+    player1_Element.classList.remove("player--active");
+};
+
+init();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = "0";
@@ -72,3 +85,5 @@ btnHold.addEventListener("click", function () {
         }
     }
 });
+
+btnNew.addEventListener("click", init);
